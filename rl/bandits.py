2d91dfbe84ed2d1169a44fa1f7b4681bf818404d
Murilo <murilovfm@gmail.com>
2018-08-28 14:52:39 -0300

    parameters for the user, total and average reward

--- rl/bandits.py
+++ rl/bandits.py
@@ -4,6 +4,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
 class bandit:
     def __init__(self,mean,variance):
@@ -15,9 +16,10 @@ class bandit:
 
 def main():
 #generate bandits
-    n_bandits = 10
+    n_bandits = int(sys.argv[1])
     bandit_list = []
     best_bandit = 0
+    np.random.seed(2)
     for i in range(n_bandits):
         this_mu = np.random.rand()*10 - 5
         this_var = np.random.rand()+.5
@@ -25,30 +27,47 @@ def main():
         if this_mu > bandit_list[best_bandit].mu:
             best_bandit = i
 
-    total_reward = [0]
+    total_reward = 0#[0]
 
 #initialize parameters
     Q = [0]*n_bandits
     N = [0]*n_bandits
-    eps = .05
-    n_repeats = 1000
+    epsilon_discount = float(sys.argv[4])
+    eps = float(sys.argv[3])
+    n_repeats = int(sys.argv[2])
     for count in range(n_repeats):
         if (np.random.rand() > eps):
             A = Q.index(max(Q))
         else:
             A = np.random.randint(n_bandits)
         reward = bandit_list[A].give_reward()
-        total_reward.append((reward+total_reward[count])/((count+1) * bandit_list[best_bandit].mu))
+        #total_reward.append((reward+total_reward[count])/((count+1) * bandit_list[best_bandit].mu))
+        total_reward += reward
         N[A] = N[A] + 1
         Q[A] = Q[A] + 1/(N[A]) * (reward - Q[A])
+        eps = eps*epsilon_discount
     for i in range(n_bandits):
         print("Estimated reward for bandit",str(i),":\t",str(Q[i]))
         print("Real reward for bandit",str(i),":\t",str(bandit_list[i].mu))
         print("Error for bandit",str(i),":\t",str(bandit_list[i].mu - Q[i]))
         print("Variance of bandit",str(i),":\t",str(bandit_list[i].sigma),"\n\n")
 
-    plt.plot(total_reward)
-    plt.show()
+    print("Total reward:",total_reward)
+    print("Average reward per step:",total_reward/n_repeats)
+
+    #plt.plot(total_reward)
+    #plt.show()
 
 if __name__ == "__main__":
-    main()
+    if (sys.argv[1] == "--help"):
+        print("Usage: python3 bandits.py p1 p2 p3 p4")
+        print("Parameters:")
+        print("\tp1: number of arms on the bandit")
+        print("\tp2: number of iterations")
+        print("\tp3: initial probability of exploration")
+        print("\tp4: discount factor on the exploration probability (p3)")
+    else:
+        if (len(sys.argv) < 5):
+            print("You should specify 4 parameters!\nFor help, use \'python3 bandits.py --help\'")
+        else:
+            main()

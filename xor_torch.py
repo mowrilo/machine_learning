858b09dc83a7fd0d5587aa43d9e54f8dc42daceb
Murilo <murilovfm@gmail.com>
2018-12-13 09:15:01 -0200

    setting seed

--- xor_torch.py
+++ xor_torch.py
@@ -10,31 +10,44 @@ y = y.reshape(4,1)
 class MLP(nn.Module):
     def __init__(self):
         super(MLP,self).__init__()
-        self.lin1 = nn.Linear(2,3,bias=True)
-        self.act1 = nn.Tanh()
-        self.lin2 = nn.Linear(3,1,bias=True)
-        self.act2 = nn.Sigmoid()
+        self.lin1 = nn.Linear(2,2,bias=True)
+        #self.act1 = nn.Tanh()
+        self.lin2 = nn.Linear(2,1,bias=True)
+        #self.act2 = nn.Sigmoid()
     def forward(self, x):
-        x = self.act1(self.lin1(x))
-        x = self.act2(self.lin2(x))
+        x = torch.tanh(self.lin1(x))
+        x = torch.sigmoid(self.lin2(x))
         return x
 
+torch.manual_seed(2)
 net = MLP()
+nn.init.constant_(net.lin1.weight,0.)
+
+for c in net.children():
+    print(c)
+    nn.init.normal_(c.weight,0.,.1)
+    nn.init.normal_(c.bias,0.,.1)
+#nn.init.constant_(net.lin1.bias,0.)
+#nn.init.constant_(net.lin2.weight,0.)
+#nn.init.constant_(net.lin2.bias,0.)
+
 y_hat = net.forward(x)
 print("Predictions before training:",(y_hat > .5).t().numpy())
 
-learning_rate = .05
+learning_rate = .1
 optimizer = optim.SGD(net.parameters(), lr=learning_rate)
 loss_func = nn.MSELoss()
 
-n_epochs = 50000
+n_epochs = 10000
 net.train()
 for i in range(n_epochs):
     optimizer.zero_grad()
     y_hat = net.forward(x)
-    loss = loss_func(y_hat, y)
+    loss = torch.pow(y_hat - y,2).sum()#loss_func(y_hat, y)
+    #print(loss)
     loss.backward(loss)
     optimizer.step()
+#    print(net.lin2.weight.data)
 net.eval()
 
 y_hat = net.forward(x)
